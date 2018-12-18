4d42120bcb5634583bbe48dfa7c738a21db08a42
Jan Kubovy <jan@kubovy.eu>
2018-12-19 00:15:56 +0100

    Minor improvement for deployment case.

--- deployment-case/src/main/java/com/poterion/monitor/notifiers/deploymentcase/control/DeploymentTask.kt
+++ deployment-case/src/main/java/com/poterion/monitor/notifiers/deploymentcase/control/DeploymentTask.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hubspot.jinjava.Jinjava
-import com.poterion.monitor.api.communication.BluetoothCommunicator
 import com.poterion.monitor.api.controllers.Service
 import com.poterion.monitor.notifiers.deploymentcase.data.Configuration
 import javafx.application.Platform
@@ -22,13 +21,21 @@ import java.util.concurrent.atomic.AtomicInteger
 import javax.net.ssl.SSLContext
 import javax.net.ssl.X509TrustManager
 
-class DeploymentTask(private val configuration: Configuration,
-					 private val communicator: BluetoothCommunicator,
-					 private val context: Map<String, Any?>,
+/**
+ * Triggers a Jenkins job, if not already running and checks its status including all downstream jobs until the root
+ * job is finished.
+ *
+ * @author Jan Kubovy <jan@kubovy.eu>
+ */
+class DeploymentTask(configuration: Configuration,
+					 context: Map<String, Any?>,
+					 private val onUpdate: (Collection<Pair<String, String>>) -> Unit,
 					 private val onFinish: () -> Unit) : Runnable {
 
 	companion object {
 		private val LOGGER: Logger = LoggerFactory.getLogger(DeploymentTask::class.java)
+		private const val NOT_RUN_YET = 0
+		private const val IN_PROGRESS = -1
 	}
 
 	private val trustAllCerts = arrayOf(object : X509TrustManager {
@@ -47,131 +54,133 @@ class DeploymentTask(private val configuration: Configuration,
 	private var isFinished = AtomicBoolean(false)
 	private var previousBuildNumber: Int = 0
 	private var status = "pending"
+	private val jobName = configuration.jobName
+	private val triggerUrl = "${configuration.url}job/${configuration.jobName}/buildWithParameters?" +
+			jinjava.render(configuration.parameters.replace("[\\n\\r]".toRegex(), ""), context)
 
 	override fun run() {
-		Platform.runLater { communicator.send("state,status,${status}") }
+		triggerUpdate("pending")
 
 		try {
-			val response = service.buildStatus(configuration.jobName).execute()
-			val body = response.body()
-			previousBuildNumber = body?.elements()
-					?.asSequence()
-					?.map { it.get("id").asInt(0) }
-					?.max()
-					?: 0
-			val lastBuildState = body?.elements()
-					?.asSequence()
-					?.find { it.get("id").asInt(0) == previousBuildNumber }
-					?.get("state")
-					?.asText()
-					?.toLowerCase()
-			if (lastBuildState == "in_progress") previousBuildNumber = -1
+			previousBuildNumber = getPreviousCompletedBuildNumber()
 
-			if (previousBuildNumber != -1) {
-				val parameters = jinjava.render(configuration.parameters.replace("[\\n\\r]".toRegex(), ""), context)
-				val url = "${configuration.url}job/${configuration.jobName}/buildWithParameters?${parameters}"
-				service.fire(url).execute()
+			if (previousBuildNumber != IN_PROGRESS) { // Trigger if currently not running
+				service.post(triggerUrl).execute()
 			}
 			updateLooper()
 			LOGGER.info("FINISHED: ${isFinished.get()} (failed ${failedCount.get()} times)")
-			status = "success"
-			Platform.runLater { communicator.send("state,status,${status}") }
+			triggerUpdate("success")
 		} catch (e: IOException) {
 			LOGGER.error("FAILED: ${isFinished.get()} (failed ${failedCount.get()} times)")
-			status = "failure"
-			Platform.runLater { communicator.send("state,status,${status}") }
+			triggerUpdate("failure")
 		} finally {
 			onFinish()
 		}
 	}
 
-	private val retrofit: Retrofit
-		get() = Retrofit.Builder()
-				.baseUrl(configuration.url)
-				.client(OkHttpClient.Builder()
-						.sslSocketFactory(SSLContext.getInstance("SSL").apply {
-							init(null, trustAllCerts, SecureRandom())
-						}.socketFactory, trustAllCerts[0])
-						.hostnameVerifier { _, _ -> true }
-						.addInterceptor { chain ->
-							val requestBuilder = chain.request().newBuilder()
+	private val retrofit: Retrofit = Retrofit.Builder()
+			.baseUrl(configuration.url)
+			.client(OkHttpClient.Builder()
+					.sslSocketFactory(SSLContext.getInstance("SSL").apply {
+						init(null, trustAllCerts, SecureRandom())
+					}.socketFactory, trustAllCerts[0])
+					.hostnameVerifier { _, _ -> true }
+					.addInterceptor { chain ->
+						val requestBuilder = chain.request().newBuilder()
 
-							if (configuration.username.isNotEmpty()) requestBuilder.header("Authorization",
-									Base64.getEncoder()
-											.encodeToString("${configuration.username}:${configuration.password}".toByteArray())
-											.let { "Basic ${it}" })
+						if (configuration.username.isNotEmpty()) requestBuilder.header("Authorization",
+								Base64.getEncoder()
+										.encodeToString("${configuration.username}:${configuration.password}".toByteArray())
+										.let { "Basic ${it}" })
 
-							val request = requestBuilder.build()
-							Service.LOGGER.debug("${request.method()} ${request.url()}...")
-							chain.proceed(request)
-						}.build())
-				.addConverterFactory(JacksonConverterFactory.create(ObjectMapper(JsonFactory()).apply {
-					disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-				}))
-				.build()
+						val request = requestBuilder.build()
+						Service.LOGGER.debug("${request.method()} ${request.url()}...")
+						chain.proceed(request)
+					}.build())
+			.addConverterFactory(JacksonConverterFactory.create(ObjectMapper(JsonFactory()).apply {
+				disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+			}))
+			.build()
 
 	private val service = retrofit.create(JenkinsRestService::class.java)
 
+	private fun getPreviousCompletedBuildNumber() = service
+			.buildStatus(jobName)
+			.execute()
+			.body()
+			?.elements()
+			?.asSequence()
+			?.map { it.get("id").asInt(0) to it.get("state").asText() }
+			?.maxBy { (buildNumber, _) -> buildNumber }
+			?.let { (buildNumber, state) -> if (state == "in_progress") IN_PROGRESS else buildNumber }
+			?: NOT_RUN_YET
+
 	private fun updateLooper() {
-		while (!isFinished.get() && failedCount.get() < 5) {
-			states.clear()
-			try {
-				val response = service.buildStatus(configuration.jobName).execute()
-				val body = response.body()
-				val lastBuildNumber = body?.elements()
+		while (!isFinished.get() && failedCount.get() < 5) try {
+			val response = service.buildStatus(jobName).execute()
+			val body = response.body()
+			val lastBuildNumber = body?.elements()
+					?.asSequence()
+					?.map { it.get("id").asInt(0) }
+					?.max()
+			LOGGER.info("Last build number: ${lastBuildNumber}, previous build number: ${previousBuildNumber}")
+			if (lastBuildNumber != previousBuildNumber) {
+				triggerUpdate("in_progress")
+				val lastBuild = body?.elements()
 						?.asSequence()
-						?.map { it.get("id").asInt(0) }
-						?.max()
-				LOGGER.info("Last build number: ${lastBuildNumber}, previous build number: ${previousBuildNumber}")
-				if (lastBuildNumber != previousBuildNumber) {
-					if (status != "in_progress") {
-						status = "in_progress"
-						Platform.runLater { communicator.send("state,status,${status}") }
+						?.find { it.get("id").asInt(0) == lastBuildNumber }
+				if (response.isSuccessful && lastBuild?.has("status") == true) {
+					failedCount.set(0)
+
+					LOGGER.debug("${jobName}: ${lastBuild.get("status").asText()}")
+					states["job_${jobName.sanitizeJobName()}"] = lastBuild
+							.get("status").asText("unknown").toLowerCase()
+
+					lastBuild.get("stages").elements().forEach { stage ->
+						val name = stage.get("name").asText().sanitizeJobName()
+
+						val status = if (stage.has("_links")
+								&& stage.get("_links").has("self")
+								&& stage.get("_links").get("self").has("href")) {
+							val url = stage.get("_links").get("self").get("href").asText()
+							try {
+								service.get(url).execute().body()
+										?.takeIf { it.has("status") }
+										?.let { it.get("status").asText("unknown") }
+										?.toLowerCase()
+										?: "unknown"
+							} catch (e: IOException) {
+								stage.get("status").asText("unknown").toLowerCase()
+							}
+						} else stage.get("status").asText("unknown").toLowerCase()
+
+						LOGGER.info("${name}: ${status}")
+						states["job_${name}"] = status
 					}
-					val lastBuild = body?.elements()
-							?.asSequence()
-							?.find { it.get("id").asInt(0) == lastBuildNumber }
-					if (response.isSuccessful && lastBuild?.has("status") == true) {
-						failedCount.set(0)
 
-						LOGGER.debug("${configuration.jobName}: ${lastBuild.get("status").asText()}")
-						states[configuration.jobName.sanitizeJobName()] = lastBuild.get("status").asText("unknown").toLowerCase()
-
-						lastBuild.get("stages").elements().forEach { stage ->
-							val name = stage.get("name").asText().sanitizeJobName()
-
-							val status = if (stage.has("_links")
-									&& stage.get("_links").has("self")
-									&& stage.get("_links").get("self").has("href")) {
-								val url = stage.get("_links").get("self").get("href").asText()
-								try {
-									service.get(url).execute().body()
-											?.takeIf { it.has("status") }
-											?.let { it.get("status").asText("unknown") }
-											?.toLowerCase()
-											?: "unknown"
-								} catch (e: IOException) {
-									stage.get("status").asText("unknown").toLowerCase()
-								}
-							} else stage.get("status").asText("unknown").toLowerCase()
-
-							LOGGER.info("${name}: ${status}")
-							states[name] = status
-						}
-
-						if (lastBuild.get("status").asText() != "IN_PROGRESS") isFinished.set(true)
-					} else {
-						failedCount.incrementAndGet()
-					}
+					if (lastBuild.get("status").asText() != "IN_PROGRESS") isFinished.set(true)
+				} else {
+					failedCount.incrementAndGet()
 				}
-			} catch (e: IOException) {
-				failedCount.incrementAndGet()
 			}
-			states.map { (name, status) -> "state,job_${name},${status}" }
-					.joinToString(";")
-					.also { Platform.runLater { communicator.send(it) } }
+		} catch (e: IOException) {
+			failedCount.incrementAndGet()
+		} finally {
+			triggerUpdate()
 			Thread.sleep(5_000)
 		}
+	}
+
+	private fun triggerUpdate(status: String? = null) {
+		val statesToSend = states.map { (k, v) -> k to v }.toMutableList()
+
+		if (status != null && this.status != status) {
+			this.status = status
+			statesToSend.add("status" to status)
+		}
+
+		Platform.runLater { onUpdate(statesToSend) }
+		states.clear()
 	}
 
 	private fun String.sanitizeJobName() = this.toLowerCase()
--- deployment-case/src/main/java/com/poterion/monitor/notifiers/deploymentcase/control/JenkinsRestService.kt
+++ deployment-case/src/main/java/com/poterion/monitor/notifiers/deploymentcase/control/JenkinsRestService.kt
@@ -8,15 +8,30 @@ import retrofit2.http.Path
 import retrofit2.http.Url
 
 /**
+ * Jenkins REST service for deployment case.
+ *
  * @author Jan Kubovy <jan@kubovy.eu>
  */
 interface JenkinsRestService {
-	@POST
-	fun fire(@Url url: String): Call<Void>
 
+	/**
+	 * Generic POST call.
+	 * @param url Full URL to call.
+	 */
+	@POST
+	fun post(@Url url: String): Call<Void>
+
+	/**
+	 * Generic GET call.
+	 * @param url Full URL to call.
+	 */
 	@GET
 	fun get(@Url url: String): Call<JsonNode>
 
+	/**
+	 * Build status call.
+	 * @param jobName Name of the job to get the status of.
+	 */
 	@GET("job/{name}/wfapi/runs")
 	fun buildStatus(@Path("name") jobName: String): Call<JsonNode>
 }
