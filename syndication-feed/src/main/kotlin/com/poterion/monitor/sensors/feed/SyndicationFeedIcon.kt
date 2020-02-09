02e5075f16c083e819ff0fd5b180261dac7be498
Jan Kubovy <jan@kubovy.eu>
2020-02-09 10:53:51 +0100

    monitor-1 Feature Request: Atom Feed Support

---
+++ syndication-feed/src/main/kotlin/com/poterion/monitor/sensors/feed/SyndicationFeedIcon.kt
@@ -0,0 +1,10 @@
+package com.poterion.monitor.sensors.feed
+
+import com.poterion.utils.javafx.Icon
+
+/**
+ * @author Jan Kubovy [jan@kubovy.eu]
+ */
+enum class SyndicationFeedIcon : Icon {
+	RSS
+}
